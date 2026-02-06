fc1a60d40d084e6f6e72442bf8df41b2761a4558
rohiths9814-cmd <rohiths9814@gmail.com>
2026-02-06 21:51:04 +0530

    first commit

---
+++ backend/src/test/java/com/localsecurecam/backend/LocalSecureCamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localsecurecam.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalSecureCamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
